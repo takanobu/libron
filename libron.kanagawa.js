aabf6d420fbad5cd9910152548d1f4c7d88a9c8a
Yukinori SUDA <sudabon@gmail.com>
2009-10-16 14:11:45 +0800

    change libron.kanagawa.js

--- libron.kanagawa.js
+++ libron.kanagawa.js
@@ -65,11 +65,8 @@ libron.kanagawa = {
         } catch(e) {
           return;
         }
-        GM_log(htmldoc.length);
         var searchId = $X('//INPUT[@TYPE="HIDDEN" and @NAME="SEARCHID"]', htmldoc);
-        GM_log('searchId='+searchId.value);
         var hitList = $X('//INPUT[@TYPE="HIDDEN" and @NAME="HITLIST"]', htmldoc);
-        GM_log('hitList='+hitList.value);
         if (searchId.length > 0 && hitList.length > 0) {
           libron.kanagawa._checkLibrary(div, 'http://www.klnet.pref.kanagawa.jp/opac/CrossServlet?HITLIST=' + hitList + '&SEARCHID=' + searchId);
         }
