67c7ed12a308803f8349d077cefdc4353103ac33
Yukinori SUDA <sudabon@gmail.com>
2009-10-16 14:29:24 +0900

    change libron.kanagawa.js

--- libron.kanagawa.js
+++ libron.kanagawa.js
@@ -65,7 +65,7 @@ libron.kanagawa = {
         } catch(e) {
           return;
         }
-        GM_log(htmldoc);
+        GM_log(htmldoc.length);
         var searchId = $X('//INPUT[@TYPE="HIDDEN" and @NAME="SEARCHID"]', htmldoc);
         GM_log('searchId='+searchId.value);
         var hitList = $X('//INPUT[@TYPE="HIDDEN" and @NAME="HITLIST"]', htmldoc);
